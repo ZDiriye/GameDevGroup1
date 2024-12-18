7b198df4abad868259329f7bfe400ac82f1c864d
ZakariyeDiriye <zakariyediriye8@gmail.com>
2024-12-18 15:57:26 +0000

    first enemy animation

---
+++ Assets/Animation/BossController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarriorController : MonoBehaviour
+{
+    Vector3 target;
+    float speed = 1.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetNewTarget(new Vector3(
+            transform.position.x + 10,
+            transform.position.y,
+            transform.position.z + 10
+        ));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = target - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+    }   
+
+    void SetNewTarget(Vector3 newTarget)
+    {
+        target = newTarget;
+        transform.LookAt(target);
+    }
+}
